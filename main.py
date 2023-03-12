bc42d95d21c06bc1b191022523aded2c8d625b31
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-03-12 20:34:09 +0400

    Fixed sizes

--- main.py
+++ main.py
@@ -11,21 +11,21 @@ class PythonWebScraper:
         # create labels and entry fields
         self.url_label = tk.Label(master, text="Enter URL:")
         self.url_label.grid(row=0, column=0, padx=5, pady=5)
-        self.url_entry = tk.Entry(master, width=60)
+        self.url_entry = tk.Entry(master, width=50)
         self.url_entry.grid(row=0, column=1, padx=5, pady=5)
 
         self.output_label = tk.Label(master, text="Output:")
         self.output_label.grid(row=1, column=0, padx=5, pady=5)
-        self.output_text = tk.Text(master, width=60, height=10)
+        self.output_text = tk.Text(master, width=50, height=10)
         self.output_text.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
 
         # create buttons
         self.scrape_button = tk.Button(
-            master, text="Scrape", command=self.scrape)
+            master, text="Scrape", command=self.scrape, width=10)
         self.scrape_button.grid(row=3, column=0, padx=5, pady=5)
 
         self.clear_button = tk.Button(
-            master, text="Clear", command=self.clear_output)
+            master, text="Clear", command=self.clear_output, width=10)
         self.clear_button.grid(row=3, column=1, padx=5, pady=5)
 
     def scrape(self):
@@ -54,6 +54,6 @@ class PythonWebScraper:
 
 
 root = tk.Tk()
-root.geometry("500x270")
+
 web_scraper_gui = PythonWebScraper(root)
 root.mainloop()
