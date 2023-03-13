8439b79d26c627db5956f349eab26bb46a756b76
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-03-13 20:18:12 +0400

    Conditions for selected option

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ class PythonWebScraper:
         self.url_label.grid(row=0, column=0, padx=5, pady=5)
         self.url_entry = tk.Entry(master, width=50)
         self.url_entry.grid(row=0, column=1, padx=5, pady=5)
-        
-        options = ["Links", "Headings"]
-        self.variable = tk.StringVar(master)
-        self.variable.set(options[0])
 
-        self.output_label = tk.Label(master, text="Option:")
+        self.options = ["Links", "Headings"]
+        self.variable = tk.StringVar(master)
+        self.variable.set(self.options[0])
+
+        self.output_label = tk.Label(master, text="Options:")
         self.output_label.grid(row=1, column=0, padx=5, pady=5)
-        
-        self.dropdown = tk.OptionMenu(root, self.variable, *options)
+
+        self.dropdown = tk.OptionMenu(root, self.variable, *self.options)
         self.dropdown.grid(row=1, column=1, padx=5, pady=5)
 
         self.output_label = tk.Label(master, text="Output:")
@@ -41,7 +41,7 @@ class PythonWebScraper:
     def scrape(self):
         # get URL from entry field
         url = self.url_entry.get()
-        
+
         # get option text
         option = self.variable.get()
 
@@ -55,12 +55,19 @@ class PythonWebScraper:
         # find all links on the webpage
         links = soup.find_all('a')
 
+        # find all headings on the webpage
+        headings = soup.find_all(['h1', 'h2', 'h3'])
+
         # clear output text
         self.output_text.delete('1.0', tk.END)
 
         # output data to text field
-        for link in links:
-            self.output_text.insert(tk.END, link.get('href') + '\n')
+        if option == self.options[0]:
+            for link in links:
+                self.output_text.insert(tk.END, link.get('href') + '\n')
+        elif option == self.options[1]:
+            for heading in headings:
+                self.output_text.insert(tk.END, heading.get_text() + '\n')
 
     def clear_output(self):
         self.output_text.delete('1.0', tk.END)
