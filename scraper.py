68e755107d0e85cae867749dae78229a150cbc68
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-07-15 19:59:46 +0400

    Updated gitignore + Add save in CSV option

--- scraper.py
+++ scraper.py
@@ -13,45 +13,65 @@ class Tab1:
 
         # create labels and entry fields
         self.url_label = tk.Label(self.frame, text="Enter URL:")
-        self.url_label.grid(row=0, column=0, padx=5, pady=5, sticky='w')
+        self.url_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
         self.url_entry = tk.Entry(self.frame, width=50)
         self.url_entry.grid(row=0, column=1, padx=5, pady=5)
 
         # dropdown options
-        self.options = ["Links", "Headings", "Images",
-                        "Paragraphs", "Meta Data", "CSS Files", "Scripts"]
+        self.options = [
+            "Links",
+            "Headings",
+            "Images",
+            "Paragraphs",
+            "Meta Data",
+            "CSS Files",
+            "Scripts",
+        ]
         self.variable = tk.StringVar(self.frame)
         self.variable.set(self.options[0])
         self.output_label = tk.Label(self.frame, text="Options:")
-        self.output_label.grid(row=1, column=0, padx=5, pady=5, sticky='w')
+        self.output_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
         self.dropdown = tk.OptionMenu(
-            self.frame, self.variable, *self.options, command=self.show_hide_checkbox)
-        self.dropdown.grid(row=1, column=1, padx=5, pady=5, sticky='w')
+            self.frame, self.variable, *self.options, command=self.show_hide_checkbox
+        )
+        self.dropdown.grid(row=1, column=1, padx=5, pady=5, sticky="w")
 
         # download images checkbox
         self.downloadImg = tk.IntVar(self.frame)
-        self.checkbox1 = tk.Checkbutton(self.frame, text="Download images", variable=self.downloadImg,
-                                        onvalue=1, offvalue=0)
+        self.checkbox1 = tk.Checkbutton(
+            self.frame,
+            text="Download images",
+            variable=self.downloadImg,
+            onvalue=1,
+            offvalue=0,
+        )
         self.checkbox1.grid_remove()
 
         # store data in file checkbox
         self.checkVariable = tk.IntVar(self.frame)
-        self.checkbox2 = tk.Checkbutton(self.frame, text="Store data in text file", variable=self.checkVariable,
-                                        onvalue=1, offvalue=0)
-        self.checkbox2.grid(row=3, column=0, padx=5, pady=5, sticky='w')
+        self.checkbox2 = tk.Checkbutton(
+            self.frame,
+            text="Store data in text file",
+            variable=self.checkVariable,
+            onvalue=1,
+            offvalue=0,
+        )
+        self.checkbox2.grid(row=3, column=0, padx=5, pady=5, sticky="w")
 
         self.output_label = tk.Label(self.frame, text="Output:")
-        self.output_label.grid(row=4, column=0, padx=5, pady=5, sticky='w')
+        self.output_label.grid(row=4, column=0, padx=5, pady=5, sticky="w")
         self.output_text = tk.Text(self.frame, width=55, height=20)
         self.output_text.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
 
         # create buttons
         self.scrape_button = tk.Button(
-            self.frame, text="Scrape", command=self.scrape, width=10)
+            self.frame, text="Scrape", command=self.scrape, width=10
+        )
         self.scrape_button.grid(row=6, column=0, padx=5, pady=5)
 
         self.clear_button = tk.Button(
-            self.frame, text="Clear", command=self.clear_output, width=10)
+            self.frame, text="Clear", command=self.clear_output, width=10
+        )
         self.clear_button.grid(row=6, column=1, padx=5, pady=5)
 
     def scrape(self):
@@ -72,69 +92,67 @@ class Tab1:
         html_content = response.content
 
         # parse HTML with BeautifulSoup
-        soup = BeautifulSoup(html_content, 'html.parser')
+        soup = BeautifulSoup(html_content, "html.parser")
 
         # find all links on the webpage
-        links = soup.find_all('a')
+        links = soup.find_all("a")
 
         # find all headings on the webpage
-        headings = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+        headings = soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6"])
 
         # find all images
-        images = soup.find_all('img')
+        images = soup.find_all("img")
 
         # find all paragraphs
-        paragraphs = soup.find_all('p')
+        paragraphs = soup.find_all("p")
 
         # scrape the meta data of the webpage
-        meta_data = soup.find_all(['meta', 'title'])
+        meta_data = soup.find_all(["meta", "title"])
 
         # scrape the css_files of the webpage
-        css_files = soup.find_all('link', attrs={"rel": "stylesheet"})
+        css_files = soup.find_all("link", attrs={"rel": "stylesheet"})
 
         # scrape the scripts of the webpage
-        scripts = soup.find_all('script')
+        scripts = soup.find_all("script")
 
         # clear output text
-        self.output_text.delete('1.0', tk.END)
+        self.output_text.delete("1.0", tk.END)
 
         # output data to text field
         if option == self.options[0]:
             for link in links:
-                self.output_text.insert(tk.END, link.get('href') + '\n')
+                self.output_text.insert(tk.END, link.get("href") + "\n")
         elif option == self.options[1]:
             for heading in headings:
-                self.output_text.insert(
-                    tk.END, heading.get_text().strip() + '\n')
+                self.output_text.insert(tk.END, heading.get_text().strip() + "\n")
         elif option == self.options[2]:
             for image in images:
-                self.output_text.insert(tk.END, image.get('src') + '\n')
+                self.output_text.insert(tk.END, image.get("src") + "\n")
         elif option == self.options[3]:
             for paragraph in paragraphs:
-                self.output_text.insert(
-                    tk.END, paragraph.get_text().strip() + '\n')
+                self.output_text.insert(tk.END, paragraph.get_text().strip() + "\n")
         elif option == self.options[4]:
             for meta in meta_data:
-                self.output_text.insert(tk.END, str(meta) + '\n')
+                self.output_text.insert(tk.END, str(meta) + "\n")
         elif option == self.options[5]:
             for css_file in css_files:
-                self.output_text.insert(tk.END, str(css_file) + '\n')
+                self.output_text.insert(tk.END, str(css_file) + "\n")
         elif option == self.options[6]:
             for script in scripts:
-                self.output_text.insert(tk.END, str(script) + '\n')
+                self.output_text.insert(tk.END, str(script) + "\n")
 
         if checked == 1:
             now = datetime.utcnow()
-            format = now.strftime('%Y%m%d%H%M')
-            with open(f'data/data_{format}.txt', 'w') as f:
+            format = now.strftime("%Y%m%d%H%M")
+            with open(f"data/data_{format}.txt", "w") as f:
                 f.write(self.output_text.get("1.0", "end-1c"))
 
         if checkedImg == 1:
             count = 1
             now = datetime.utcnow()
-            format = now.strftime('%Y%m%d%H%M')
+            format = now.strftime("%Y%m%d%H%M")
 
-            directory = 'images/' + str(format) + "/"
+            directory = "images/" + str(format) + "/"
 
             # create the directory if it doesn't already exist
             if not os.path.exists(directory):
@@ -142,13 +160,13 @@ class Tab1:
 
             # loop through each image and download it
             for image in images:
-                url = image.get('src')
+                url = image.get("src")
 
                 # send a GET request to the URL to download the image
                 response = requests.get(url)
 
                 # construct the file name to save the image as
-                filename = os.path.join(directory, 'image{}'.format(count))
+                filename = os.path.join(directory, "image{}".format(count))
 
                 # use os.path.splitext to split the filename into base name and extension
                 _, extension = os.path.splitext(url)
@@ -156,7 +174,7 @@ class Tab1:
                 print(filename)
 
                 # save the image to the chosen file path
-                with open(f'{filename}{extension}', 'wb') as f:
+                with open(f"{filename}{extension}", "wb") as f:
                     f.write(response.content)
                     count += 1
 
@@ -167,7 +185,7 @@ class Tab1:
     def show_hide_checkbox(self, _=None):
         value = self.variable.get()
         if value == self.options[2]:
-            self.checkbox1.grid(row=2, column=0, padx=5, pady=5, sticky='w')
+            self.checkbox1.grid(row=2, column=0, padx=5, pady=5, sticky="w")
         else:
             self.downloadImg.set(0)
             self.checkbox1.grid_remove()
--- table_data.py
+++ table_data.py
@@ -1,3 +1,5 @@
+import csv
+from datetime import datetime
 import tkinter as tk
 from tkinter import ttk
 import requests
@@ -11,48 +13,80 @@ class Tab2:
 
         # create labels and entry fields
         self.url_label = tk.Label(self.frame, text="Enter URL:")
-        self.url_label.grid(row=0, column=0, padx=5, pady=5, sticky='w')
-        self.url_entry = tk.Entry(self.frame, width=60)
+        self.url_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        self.url_entry = tk.Entry(self.frame, width=50)
         self.url_entry.grid(row=0, column=1, padx=5, pady=5)
+
+        # store data in CSV checkbox
+        self.checkVariable = tk.IntVar(self.frame)
+        self.checkbox = tk.Checkbutton(
+            self.frame,
+            text="Store data in CSV file",
+            variable=self.checkVariable,
+            onvalue=1,
+            offvalue=0,
+            command=self.show_hide_checkbox,
+        )
+        self.checkbox.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+
+        # include header checkbox
+        self.includeHeader = tk.IntVar(self.frame)
+        self.csvHeader = tk.Checkbutton(
+            self.frame,
+            text="Include header in CSV",
+            variable=self.includeHeader,
+            onvalue=1,
+            offvalue=0,
+        )
+        self.csvHeader.grid_remove()
 
         # create output table
         self.treeview = ttk.Treeview(self.frame, columns=(), height=20)
-        self.treeview.grid(row=1, column=0, columnspan=2,
-                           padx=5, pady=5, sticky=tk.NSEW)
+        self.treeview.grid(
+            row=3, column=0, columnspan=2, padx=5, pady=5, sticky=tk.NSEW
+        )
 
         # create button
         self.scrape_button = tk.Button(
-            self.frame, text="Scrape", command=self.scrape, width=10)
-        self.scrape_button.grid(row=2, column=0, padx=5, pady=5)
+            self.frame, text="Scrape", command=self.scrape, width=10
+        )
+        self.scrape_button.grid(row=4, column=0, padx=5, pady=5)
 
         self.clear_button = tk.Button(
-            self.frame, text="Clear", command=self.clear_output, width=10)
-        self.clear_button.grid(row=2, column=1, padx=5, pady=5)
+            self.frame, text="Clear", command=self.clear_output, width=10
+        )
+        self.clear_button.grid(row=4, column=1, padx=5, pady=5)
 
     def scrape(self):
         # get URL from entry field
         url = self.url_entry.get()
+
+        # get checkbox state
+        checked = self.checkVariable.get()
+
+        # get includeHeader checkbox state
+        includeHeader = self.includeHeader.get()
 
         # make request to website
         response = requests.get(url)
         html_content = response.content
 
         # parse HTML with BeautifulSoup
-        soup = BeautifulSoup(html_content, 'html.parser')
+        soup = BeautifulSoup(html_content, "html.parser")
 
         # find table element
-        table = soup.find('table')
+        table = soup.find("table")
 
         # create table header
         table_header = []
-        for th in table.find_all('th'):
+        for th in table.find_all("th"):
             table_header.append(th.text.strip())
 
         # create table rows
         table_rows = []
-        for tr in table.find_all('tr'):
+        for tr in table.find_all("tr"):
             table_row = []
-            for td in tr.find_all('td'):
+            for td in tr.find_all("td"):
                 table_row.append(td.text.strip())
             table_rows.append(table_row)
 
@@ -60,13 +94,38 @@ class Tab2:
         self.treeview.delete(*self.treeview.get_children())
 
         # populate table in treeview widget
-        self.treeview['columns'] = table_header
-        self.treeview['show'] = 'headings'
+        self.treeview["columns"] = table_header
+        self.treeview["show"] = "headings"
         for header in table_header:
             self.treeview.heading(header, text=header)
             self.treeview.column(header)
         for row in table_rows:
-            self.treeview.insert('', tk.END, values=row)
+            self.treeview.insert("", tk.END, values=row)
+
+        if checked == 1:
+            now = datetime.utcnow()
+            format = now.strftime("%Y%m%d%H%M")
+            with open(f"csv/csv_{format}.csv", "w") as f:
+                csvwriter = csv.writer(f, delimiter=",")
+
+                if includeHeader == 1:
+                    print("save header:", table_header)
+                    csvwriter.writerow(table_header)
+
+                for row_id in self.treeview.get_children():
+                    row = self.treeview.item(row_id)["values"]
+                    if row != "":
+                        print("save row:", row)
+                        csvwriter.writerow(row)
 
     def clear_output(self):
         self.treeview.delete(*self.treeview.get_children())
+
+    # function to show/hide checkbox based on entry value
+    def show_hide_checkbox(self, _=None):
+        value = self.checkVariable.get()
+        if value == 1:
+            self.csvHeader.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+        else:
+            self.includeHeader.set(0)
+            self.csvHeader.grid_remove()
